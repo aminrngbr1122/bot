76b26262decd2cf3a9f1510ec68c0d4652bf3999
A M I N J A S K <102389207+aminrngbr1122@users.noreply.github.com>
2023-02-09 02:27:43 +0330

    s
    s

--- answer.py
+++ answer.py
@@ -1,12 +1,20 @@
-from bot import send_message, send_photo, send_audio, send_video, TOKEN
-from subprocess import getoutput
+from bot import send_message, send_photo, send_audio, send_video
+import re
+
+
+def is_link(text):
+    # Regular expression pattern to match a URL
+    url_pattern = re.compile(
+        r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+')
+
+    # Use the `re.search` method to check if the text matches the URL pattern
+    return bool(re.search(url_pattern, text))
+
 
 def yourbotcode(chat_id, text):
-    # 
-    if text == '/start':
-        send_message(chat_id, "Code >")
+    #
+    if is_link(text):
+        send_video(chat_id, text)
     else:
-        send_message(chat_id, 'Please Wait ...')
-        get = getoutput(f'{text}')
-        send_message(chat_id, get)
-    # 
+        pass
+    #
--- bot.py
+++ bot.py
@@ -3,67 +3,82 @@ import sys
 import requests
 import answer
 
-TOKEN = input('Token : ')
 
-BASE_URL = "https://api.telegram.org/bot{0}/".format(TOKEN)
-
-def get_updates(offset=None):
-    url = BASE_URL + "getUpdates"
-    if offset:
-        url += "?offset={}".format(offset)
-    response = requests.get(url)
-    return json.loads(response.content.decode("utf8"))
-
-def get_last_update_id(updates):
-    update_ids = []
-    for update in updates["result"]:
-        update_ids.append(int(update["update_id"]))
-    return max(update_ids)
+def is_telegram_bot_token(text):
+    if len(text) != 43:
+        return False
+    for char in text:
+        if not char.isalnum():
+            return False
+    return True
 
 
-# Bot Function
-# ============================================
+TOKEN = input('Token :')
 
-def send_message(chat_id, text):
-    url = BASE_URL + "sendMessage"
-    data = {"chat_id": chat_id, "text": text}
-    response = requests.post(url, data=data)
-    return json.loads(response.content.decode("utf8"))
+if is_telegram_bot_token(TOKEN):
 
-def send_photo(chat_id, url):
-    url = BASE_URL + "sendphoto"
-    data = {"chat_id": chat_id, "photo": url}
-    response = requests.post(url, data=data)
-    return json.loads(response.content.decode("utf8"))
+    BASE_URL = "https://api.telegram.org/bot{0}/".format(TOKEN)
 
-def send_video(chat_id, url):
-    url = BASE_URL + "sendvideo"
-    data = {"chat_id": chat_id, "video": url}
-    response = requests.post(url, data=data)
-    return json.loads(response.content.decode("utf8"))
+    def get_updates(offset=None):
+        url = BASE_URL + "getUpdates"
+        if offset:
+            url += "?offset={}".format(offset)
+        response = requests.get(url)
+        return json.loads(response.content.decode("utf8"))
 
-def send_audio(chat_id, url):
-    url = BASE_URL + "sendaudio"
-    data = {"chat_id": chat_id, "audio": url}
-    response = requests.post(url, data=data)
-    return json.loads(response.content.decode("utf8"))
+    def get_last_update_id(updates):
+        update_ids = []
+        for update in updates["result"]:
+            update_ids.append(int(update["update_id"]))
+        return max(update_ids)
 
-# =============================================
-# End Bot Function
+    # Bot Function
+    # ============================================
 
-def handle_updates(updates):
-    for update in updates["result"]:
-        text = update["message"]["text"]
-        chat_id = update["message"]["chat"]["id"]
-        answer.yourbotcode(chat_id, text)
-        
-def main():
-    last_update_id = None
-    while True:
-        updates = get_updates(last_update_id)
-        if len(updates["result"]) > 0:
-            last_update_id = get_last_update_id(updates) + 1
-            handle_updates(updates)
+    def send_message(chat_id, text):
+        url = BASE_URL + "sendMessage"
+        data = {"chat_id": chat_id, "text": text}
+        response = requests.post(url, data=data)
+        return json.loads(response.content.decode("utf8"))
 
-if __name__ == '__main__':
-    main()
+    def send_photo(chat_id, url):
+        url = BASE_URL + "sendphoto"
+        data = {"chat_id": chat_id, "photo": url}
+        response = requests.post(url, data=data)
+        return json.loads(response.content.decode("utf8"))
+
+    def send_video(chat_id, url):
+        url = BASE_URL + "sendvideo"
+        data = {"chat_id": chat_id, "video": url}
+        response = requests.post(url, data=data)
+        return json.loads(response.content.decode("utf8"))
+
+    def send_audio(chat_id, url):
+        url = BASE_URL + "sendaudio"
+        data = {"chat_id": chat_id, "audio": url}
+        response = requests.post(url, data=data)
+        return json.loads(response.content.decode("utf8"))
+
+    # =============================================
+    # End Bot Function
+
+    def handle_updates(updates):
+        for update in updates["result"]:
+            text = update["message"]["text"]
+            chat_id = update["message"]["chat"]["id"]
+            answer.yourbotcode(chat_id, text)
+
+    def main():
+        last_update_id = None
+        while True:
+            updates = get_updates(last_update_id)
+            if len(updates["result"]) > 0:
+                last_update_id = get_last_update_id(updates) + 1
+                handle_updates(updates)
+
+    if __name__ == '__main__':
+        main()
+
+else:
+
+    sys.exit()
