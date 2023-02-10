159616311fdacc289eaa5b28e4857de1c32003c2
A M I N J A S K <102389207+aminrngbr1122@users.noreply.github.com>
2023-02-11 02:20:05 +0330

    Update answer.py

--- answer.py
+++ answer.py
@@ -12,17 +12,8 @@ from bs4 import BeautifulSoup
 
 def yourbotcode(chat_id, text):
     #
-    if ( text == '/start' ) :
-    	url = 'http://sexporn.pics'
-    	response = requests.get(url)
-    	soup = BeautifulSoup(response.content, 'html.parser')
-    	img_tags = soup.find_all('img')
-    	urls = [img['src'] for img in img_tags]
-    	for i, url in enumerate(urls):
-    	       try:
-    	       	send_photo(chat_id, url)
-    	       except:
-    	       	pass
+    if text == '/start':
+    	pass
     else:
     	send_message(chat_id, '/start')
     #
