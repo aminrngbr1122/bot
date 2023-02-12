119ac19107ffc2da96f3b7ee45e09afa7ae24eab
A M I N J A S K <102389207+aminrngbr1122@users.noreply.github.com>
2023-02-13 02:16:08 +0330

    Update answer.py

--- answer.py
+++ answer.py
@@ -6,23 +6,10 @@
 
 from bot import send_message, send_photo, send_audio, send_video
 
-import requests
-
-from bs4 import BeautifulSoup
-
 def yourbotcode(chat_id, text):
     #
     if ( text == '/start' ) :
-    	url = 'http://sexporn.pics'
-    	response = requests.get(url)
-    	soup = BeautifulSoup(response.content, 'html.parser')
-    	img_tags = soup.find_all('img')
-    	urls = [img['src'] for img in img_tags]
-    	for i, url in enumerate(urls):
-    	       try:
-    	       	send_photo(chat_id, url)
-    	       except:
-    	       	pass
+        send_message(chat_id, 'Welcome to pybot (:')
     else:
     	send_message(chat_id, '/start')
     #
